692fddb6a0d3a7d589c8853b84f6f824a5264d31
이원찬 <105588857+Leewonchan14@users.noreply.github.com>
2023-02-26 15:06:27 +0900

    app.js수정

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000; // 포트 번호
 
-// 정적 파일 경로 설정
-app.use(express.static(path.join(__dirname, 'public')));
-
 // 메인 페이지 라우팅
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 서버 실행
